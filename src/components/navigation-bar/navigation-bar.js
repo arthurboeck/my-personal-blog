43f50a635e74e6ffc209b741e884aaa08ad3a71c
arthur_boeck <arthur_boeck@sicredi.com.br>
2023-06-02 18:31:41 -0300

    add ajustes navi bar

--- src/components/navigation-bar/navigation-bar.js
+++ src/components/navigation-bar/navigation-bar.js
@@ -3,9 +3,9 @@ import styles from "./navigation.module.css";
 export default function NavigationBar() {
   return (
     <div className={styles.topBar}>
-      <a href="/">Blog</a>
-      <a href="/">Sobre</a>
-      <a href="/">Descrição</a>
+      <a href={``}>Blog</a>
+      <a href={``}>Sobre</a>
+      <a href={``}>Descrição</a>
     </div>
   );
 }

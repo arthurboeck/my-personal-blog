7259284d36f0c8f9ac1d84bb4dcf15edc8847946
arthur_boeck <arthur_boeck@sicredi.com.br>
2023-06-01 22:23:09 -0300

    add top bar nav

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,49 +1,55 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from './layout.module.css'
-import utilStyles from '../styles/utils.module.css'
-import Link from 'next/link'
+import Image from "next/image";
+import Link from "next/link";
+import utilStyles from "../styles/utils.module.css";
+import styles from "./layout.module.css";
 
-const name = 'Arthur Guterres Boeck'
-export const siteTitle = 'My first blog page'
+const name = "Arthur Guterres Boeck";
+export const siteTitle = "My first blog page";
 
 export default function Layout({ children, home }) {
   return (
-    <div className={styles.container}>
-      <header className={styles.header}>
-        {home ? (
-          <>
-            <Image
-              priority
-              src="/images/profile.jpg"
-              className={utilStyles.borderCircle}
-              height={144}
-              width={144}
-              alt={name}
-            />
-            <h1 className={utilStyles.heading2Xl}>{name}</h1>
-          </>
-        ) : (
-          <>
-            <Link href="/">
+    <div>
+      <div className={utilStyles.topBar}>
+        <a href="/">Blog</a>
+        <a href="/">Sobre</a>
+        <a href="/">Descrição</a>
+      </div>
+      <div className={styles.container}>
+        <header className={styles.header}>
+          {home ? (
+            <>
               <Image
                 priority
                 src="/images/profile.jpg"
                 className={utilStyles.borderCircle}
-                height={108}
-                width={108}
+                height={144}
+                width={144}
                 alt={name}
               />
-            </Link>
-            <h2 className={utilStyles.headingLg}>
-              <Link href="/" className={utilStyles.colorInherit}>
-                {name}
+              <h1 className={utilStyles.heading2Xl}>{name}</h1>
+            </>
+          ) : (
+            <>
+              <Link href="/">
+                <Image
+                  priority
+                  src="/images/profile.jpg"
+                  className={utilStyles.borderCircle}
+                  height={108}
+                  width={108}
+                  alt={name}
+                />
               </Link>
-            </h2>
-          </>
-        )}
-      </header>
-      <main>{children}</main>
+              <h2 className={utilStyles.headingLg}>
+                <Link href="/" className={utilStyles.colorInherit}>
+                  {name}
+                </Link>
+              </h2>
+            </>
+          )}
+        </header>
+        <main>{children}</main>
+      </div>
     </div>
-  )
+  );
 }

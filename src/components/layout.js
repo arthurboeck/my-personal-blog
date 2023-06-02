311b73a3237cc2e1f0e9fbbaf7bd3c54694d8579
arthur_boeck <arthur_boeck@sicredi.com.br>
2023-06-02 18:31:16 -0300

    add divisao das telas

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,10 +8,14 @@ export const siteTitle = "My first blog page";
 export default function Layout({ children }) {
   return (
     <div>
-      <NavigationBar />
-      <div className={styles.container}>
-        <ProfileHeader home />
-        <ProfileInfo />
+      <div className={styles.colouredBackGround}>
+        <NavigationBar />
+        <div className={styles.container}>
+          <ProfileHeader home />
+          <ProfileInfo />
+        </div>
+      </div>
+      <div className={styles.whiteBackGround}>
         <main>{children}</main>
       </div>
     </div>

56a7c2a885fc8b5b42b483cdc016dbd63b944b1c
Arthur Boeck <arthurguterres@outlook.com>
2023-07-18 18:35:51 -0300

    melhorias de codigo bot reviewer

--- src/components/layout.test.js
+++ src/components/layout.test.js
@@ -2,21 +2,23 @@ import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
 import Layout from './layout';
 
+const layoutComponent = <Layout />;
+
 describe('Layout', () => {
   test('renders navigation bar', () => {
-    render(<Layout />);
+    render(layoutComponent);
     const navigationBar = screen.getByTestId('navigation-bar');
     expect(navigationBar).toBeInTheDocument();
   });
 
   test('renders profile header', () => {
-    render(<Layout />);
+    render(layoutComponent);
     const profileHeader = screen.getByTestId('profile-header');
     expect(profileHeader).toBeInTheDocument();
   });
 
   test('renders profile info', () => {
-    render(<Layout />);
+    render(layoutComponent);
     const profileInfo = screen.getByTestId('profile-info');
     expect(profileInfo).toBeInTheDocument();
   });
--- src/components/profile-header/profile-header.test.js
+++ src/components/profile-header/profile-header.test.js
@@ -2,37 +2,36 @@ import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
 import ProfileHeader from './profile-header';
 
+beforeEach(() => {
+  render(<ProfileHeader />);
+});
+
 describe('ProfileHeader', () => {
-  describe('ProfileHeader', () => {
-    test('renders profile header', () => {
-      render(<ProfileHeader />);
-      const profileHeader = screen.getByTestId('profile-header');
-      expect(profileHeader).toBeInTheDocument();
-    });
+  test('renders profile header', () => {
+    const profileHeader = screen.getByTestId('profile-header');
+    expect(profileHeader).toBeInTheDocument();
+  });
 
-    test('renders profile image', () => {
-      render(<ProfileHeader />);
-      const profileImage = screen.getByRole(
-          'img',
-          { name: /Arthur Guterres Boeck/u },
-      );
+  test('renders profile image', () => {
+    const profileImage = screen.getByRole(
+        'img',
+        { name: /Arthur Guterres Boeck/u },
+    );
 
-      expect(profileImage).toBeInTheDocument();
-    });
+    expect(profileImage).toBeInTheDocument();
+  });
 
-    test('renders profile name', () => {
-      render(<ProfileHeader />);
-      const data = {
-        level: 1,
-        name: /Arthur Guterres Boeck/u,
-      };
+  test('renders profile name', () => {
+    const data = {
+      level: 1,
+      name: /Arthur Guterres Boeck/u,
+    };
 
-      const profileName = screen.getByRole(
-          'heading',
-          data,
-      );
+    const profileName = screen.getByRole(
+        'heading',
+        data,
+    );
 
-      expect(profileName).toBeInTheDocument();
-    });
+    expect(profileName).toBeInTheDocument();
   });
 });
